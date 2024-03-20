e84d9974f91485c018b08261ab5eaafad136444f
Iago024 <iagoantoniacomi0@gmail.com>
2024-03-19 22:59:27 -0300

    desafio-encriptador-de-texto

--- js/app.js
+++ js/app.js
@@ -6,34 +6,17 @@ let saida = document.getElementById('saida');
 
 function criptografarTexto(texto, chave){
   let textoEncriptado = '';
-  for(let i = 0; i < texto.length; i++){
-    let caractere = texto[i];
-
-    if(caractere.match(/[a-z]/i)){
-      let codigoAsc = texto.charCodeAt(i);
-
-      if(codigoAsc >= 65 && codigoAsc <= 90){
-        caractere = String.fromCharCode(((codigoAsc - 65 + chave) % 26) + 65);
-      } else if(codigoAsc >= 97 && codigoAsc <= 122){
-        caractere = String.fromCharCode(((codigoAsc - 97 + chave) % 26) + 97);
-      } else {
-        saida.innerHTML = 'Só é aceito letras maiúsculas e minúsculas e sem acento';
-      }
-    }
-    textoEncriptado += caractere; 
-  }
+  
 
   return textoEncriptado;
 }
 
 function descriptografarTexto(texto, chave){
-  return criptografarTexto(texto, (26 - chave) % 26);
 }
 
 function criptografar(){
   let msgInput = entrada.value;
-  const deslocamento = 7;
-  let criptografado = criptografarTexto(msgInput, deslocamento);
+  let criptografado = criptografarTexto();
 
   saida.value = criptografado;
 
